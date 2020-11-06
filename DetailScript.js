5e5f0215cf5e8f055a5d85be305570fd447275d9
TianyouZ <tianyouzhang@outlook.com>
2020-11-06 12:34:53 +0800

    Initial Commit

---
+++ DetailScript.js
@@ -0,0 +1,18 @@
+
+    var x = 0;
+    function myFunction() {
+  		document.getElementById("mainP").src = "Color_1.JPG";
+	}
+
+    function myFunction2() {
+  		document.getElementById("mainP").src = "Color_2.JPG";
+	}
+
+    function myFunction3() {
+  		document.getElementById("mainP").src = "Color_3.JPG";
+	}
+    
+        function myFunctionC() {
+    	x++;
+  		document.getElementById("Cart").innerHTML = "Cart(" + x +")";
+	}
